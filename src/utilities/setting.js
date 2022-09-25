091319ef8ebc2b467b0aee276c4d2e518c8dcff3
enidev911 <enidev911@gmail.com>
2022-09-25 19:24:53 -0300

    up-col-xl

--- src/utilities/setting.js
+++ src/utilities/setting.js
@@ -2,7 +2,7 @@ const customClass = {
   container: 'bg-dark bg-gradient',
   title: 'fs-3 text-warning',
   image: 'w-75 rounded-pill border',
-  popup: 'bg-dark bg-gradient border p-0 col-10 col-md-8 col-lg-4',
+  popup: 'bg-dark bg-gradient border p-0 col-10 col-md-6 col-lg-4 col-xl-3',
   footer: 'bg-dark p-2',
   confirmButton: 'bg-danger fs-5 p-2'
 }
